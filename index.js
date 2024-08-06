d5631bb53c41dbb5c7875913d8a3e80941a3b76d
Timo von Holtz <timo@neurelo.com>
2024-08-06 20:31:12 +0800

    Add TS code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 import "dotenv/config";
+
+import { UsersApiService } from 'neurelo-sdk';
+
+async function execute() {
+	const response = await UsersApiService.findUsers(		
+		{"id":true},
+		undefined,
+		undefined,
+		undefined,
+		undefined
+	);
+
+	console.log("response: ", response.data?.data);
+
+	return response;
+}
+
+execute();
